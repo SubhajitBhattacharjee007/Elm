804a591464ab45adb199ecd360c7d68e33b99fbb
SubhajitHelloWorld <subhajitbhattacharjee007@gmail.com>
2024-03-30 17:22:54 +0530

    Revert "Merge pull request #5 from SubhajitBhattacharjee007/pr_testing"
    This reverts commit d1dcbd132bfa5b37d4825a1a216d37065cb25590, reversing
    changes made to ca935531f3a95f9f7a1112b824f995c013fe864e.

--- src/components/BackgroundOverlay/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./index.css"
-
-
-const BackgroundOverlay = ()=>{
-    <div className="overlay"></div>;
-}
-
-export default BackgroundOverlay;
--- src/templates/DashboardTemplate.js
+++ src/templates/DashboardTemplate.js
@@ -1,11 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, {useState } from "react";
+import React, { useState } from "react";
 import Task from "../components/Task";
 import "../componentStyling/task.css";
-import Dashboard from "../components/ModalRender";
-
-
-
 
 function DashboardTemplate() {
   const [open, setOpen] = useState(false);
@@ -14,9 +10,8 @@ function DashboardTemplate() {
   return (
     <div className="DashboardTemplate">
       {!open && <h2>Dashboard is empty.</h2>}
-      
-      {/* {!open && <button className="createTasksbtn" onClick={() => setOpen(true)}>Create some tasks.</button>} */}
-      <Dashboard/>
+      {!open && <a onClick={() => setOpen(true)}>+ Create some tasks.</a>}
+      {open && <Task />}
     </div>
   );
 }

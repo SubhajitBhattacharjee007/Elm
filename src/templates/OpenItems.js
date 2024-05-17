2e04de86e7f34ad7dc2eda0b093be0f5a739c309
SubhajitHelloWorld <subhajitbhattacharjee007@gmail.com>
2024-05-18 01:38:44 +0530

    update

--- src/templates/OpenItems.js
+++ src/templates/OpenItems.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { FcApproval } from "react-icons/fc";
 import { SlClose } from "react-icons/sl";
 import "../componentStyling/ClosedItemTemplate.css";
-import { BiExpand } from "react-icons/bi";
+import { SlSizeFullscreen } from "react-icons/sl";
+import { FcOk } from "react-icons/fc";
 
 function OpenItems(props) {
   const ticket = props.ticket;
@@ -11,13 +11,13 @@ function OpenItems(props) {
       <div class="grid-item-OpenTemplate">
         <div class="TicketHeader">
           <div class="TicketHeader-openTicket-flex1">
-            {ticket.status === "Closed" ? <SlClose /> : <FcApproval />}
+            {ticket.status === "Closed" ? <SlClose /> : <FcOk />}
           </div>
           <div class="TicketHeader-openTicket-flex2">{ticket.id}</div>
           <div class="TicketHeader-openTicket-flex3">{ticket.status}</div>
           <div class="TicketHeader-openTicket-flex4">{ticket.priority}</div>
           <div class="TicketHeader-openTicket-flex5">
-            <BiExpand
+            <SlSizeFullscreen
               onClick={() => {
                 props.setOpenSelectedTicket(true);
                 props.setSelectedTicket(ticket);
